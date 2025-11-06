8cced9f2221703a5083c97dd1c952b4df6f318f9
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-11-06 06:09:26 -0300

    Primeiro commit

---
+++ produtos/src/test/java/io/github/projetoparadesenvolvimento/icompras/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.projetoparadesenvolvimento.icompras.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
