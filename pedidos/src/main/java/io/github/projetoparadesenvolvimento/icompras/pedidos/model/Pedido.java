a4e554a2c1f72a9f6a0139767b4cf2b32250d182
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-11-17 15:54:32 -0300

    Created class ItemPedido, Pedido and Enums for Pedido

---
+++ pedidos/src/main/java/io/github/projetoparadesenvolvimento/icompras/pedidos/model/Pedido.java
@@ -0,0 +1,48 @@
+package io.github.projetoparadesenvolvimento.icompras.pedidos.model;
+
+import io.github.projetoparadesenvolvimento.icompras.pedidos.model.enums.StatusPedido;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "pedido")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(name = "codigo_cliente", nullable = false)
+    private Long codigoCliente;
+
+    @Column(name = "data_pedido", nullable = false)
+    private LocalDateTime dataPedido;
+
+    @Column(name = "total",precision = 16, scale = 2, nullable = false)
+    private BigDecimal total;
+
+    @Column(name = "chave_pagamento")
+    private String chavePagamento;
+
+    @Column(name = "observacoes")
+    private String observacoes;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private StatusPedido statusPedido;
+
+    @Column(name = "codigo_rastreio")
+    private String codigoRastreio;
+
+    @Column(name = "url_nf")
+    private String urlNotaFiscal;
+}
---
+++ pedidos/src/main/java/io/github/projetoparadesenvolvimento/icompras/pedidos/model/enums/StatusPedido.java
@@ -0,0 +1,11 @@
+package io.github.projetoparadesenvolvimento.icompras.pedidos.model.enums;
+
+public enum StatusPedido {
+    REALIZADO,
+    PAGO,
+    FATURADO,
+    ENVIADO,
+    ERRO,
+    ERRO_PAGAMENTO,
+    PREPARANDO_ENVIO
+}
